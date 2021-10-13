f551992778de430826c080dc536d8b6117a9f2f0
yeah <1159161880@qq.com>
2021-10-13 16:38:45 +0800

    初始化项目

---
+++ src/main/java/com/example/demo/DddczGameHouseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddczGameHouseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddczGameHouseServerApplication.class, args);
+	}
+
+}
