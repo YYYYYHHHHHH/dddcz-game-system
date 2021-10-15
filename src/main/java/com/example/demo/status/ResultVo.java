809159af60238e97b3b931a095e505bbce4dc177
yeah <1159161880@qq.com>
2021-10-15 17:38:53 +0800

    ResultVo 添加getter和setter

--- src/main/java/com/example/demo/status/ResultVo.java
+++ src/main/java/com/example/demo/status/ResultVo.java
@@ -44,4 +44,37 @@ public class ResultVo {
         this.success = statusCode.getSuccess();
         this.data = null;
     }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
 }
