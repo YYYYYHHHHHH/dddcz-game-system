549477d9d177395776853dd8f82816f47f44d373
yeah <1159161880@qq.com>
2021-10-16 16:15:02 +0800

    上传图片接口

--- src/main/java/com/example/demo/controller/AuthUserController.java
+++ src/main/java/com/example/demo/controller/AuthUserController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import com.example.demo.entity.AuthUser;
 import com.example.demo.exception.UserException;
@@ -8,6 +11,7 @@ import com.example.demo.serviceImpl.AuthUserServiceImpl;
 import com.example.demo.status.UserResultCode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +19,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController()
 @RequestMapping("/api/user")
+
 public class AuthUserController {
     @Autowired
     private AuthUserServiceImpl authUserService;
@@ -52,4 +60,26 @@ public class AuthUserController {
         return authUserService.editUser(user);
     }
 
+    // 注入配置中图片保存路径
+    @Value("${user.filepath}")
+    private String filePath;
+
+    @PostMapping(value = "/upload", consumes = "multipart/form-data")
+    String uploadAvatar(@RequestPart("avatar") MultipartFile multipartFile, @RequestParam("id") int id)
+            throws IllegalStateException, IOException {
+        UUID uuid = UUID.randomUUID();
+        String originalFileName = multipartFile.getOriginalFilename();
+        String fileSuffix = originalFileName.substring(originalFileName.lastIndexOf('.'));
+
+        File file = new File(filePath + uuid + fileSuffix);
+        multipartFile.transferTo(file);
+
+        String avatarUrl = "http://localhost:8080/api/images/" + uuid + fileSuffix;
+        AuthUser user = new AuthUser();
+        user.setAvatar(avatarUrl);
+        user.setId(id);
+
+        authUserService.editUser(user);
+        return avatarUrl;
+    }
 }
